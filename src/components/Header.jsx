7c58958d4400d47b97e5421cdc4e274a3003c2b2
ozkan <ozkandnz09@gmail.com>
2024-10-27 12:43:36 +0300

    card responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/nba-logo.png";
 const Header = () => {
   return (
     <Container>
-      <Image src={logo} width={200}/>;
+      <Image src={logo} width={200}/>
       <h1>NBA Legends</h1>
     </Container>
   );
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,18 +1,21 @@
+import { Col } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 
 const PlayerCard = ({ legends }) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={legends.img} />
-      <Card.Body>
-        <Card.Title>{legends.name}</Card.Title>
-        <ul>
-          {legends.statistics.map((item) => (
-            <li>{item}</li>
-          ))}
-        </ul>
-      </Card.Body>
-    </Card>
+    <Col xs={10} sm={8} md={6} lg={4} xl={3} >
+      <Card>
+        <Card.Img variant="top" src={legends.img} />
+        <Card.Body>
+          <Card.Title>{legends.name}</Card.Title>
+          <ul>
+            {legends.statistics.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
