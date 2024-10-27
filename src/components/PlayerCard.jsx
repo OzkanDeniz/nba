497f0c9c1d61f7a21882f2ab97458bc469fefafe
ozkan <ozkandnz09@gmail.com>
2024-10-27 13:45:37 +0300

    card useState

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import { Col } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 
 const PlayerCard = ({ legends }) => {
+  const [show, setshow] = useState(true);
+
   return (
     <Col xs={10} sm={8} md={6} lg={4} xl={3}>
       <Card>
-        <Card.Img variant="top" src={legends.img} />
-        <Card.Body>
-          <ul>
-            {legends.statistics.map((item) => (
-              <li className="list-unstyled h5 text-start">🏀{item}</li>
-            ))}
-          </ul>
-        </Card.Body>
+        {show ? (
+          <Card.Img onClick={()=>setshow(false)} variant="top" src={legends.img} />
+        ) : (
+          <Card.Body onClick={()=>setshow(true)}>
+            <ul>
+              {legends.statistics.map((item) => (
+                <li className="list-unstyled h5 text-start">🏀{item}</li>
+              ))}
+            </ul>
+          </Card.Body>
+        )}
+
         <Card.Footer>{legends.name}</Card.Footer>
       </Card>
     </Col>
