47c4ed5527e8fb3a93ca8f5f04642d91ee178433
ozkan <ozkandnz09@gmail.com>
2024-10-27 16:33:30 +0300

    filteredData

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,15 +1,33 @@
 import { Container, Form, Row } from "react-bootstrap";
 import { data } from "../helper/data";
 import PlayerCard from "./PlayerCard";
+import { useState } from "react";
 
 const CardContainer = () => {
+  const [search, setsearch] = useState("");
+
+  const handleChange = (e) => {
+    setsearch(e.target.value);
+    
+  };
+
+const filteredData = data.filter(legends=> legends.name.toLocaleLowerCase().includes(search.trim().toLocaleLowerCase()))
+console.log(filteredData)
+
+
   return (
     <>
-      <Form.Control type="search" placeholder="Search Player..." /><br/>
+      <Form.Control
+        type="search"
+        onChange={(e) => handleChange(e)}
+        placeholder="Search Player..."
+      />
+      <br />
+
       <Container>
         <Row>
           {data.map((legends) => (
-            <PlayerCard legends={legends} />
+            <PlayerCard key={legends.id} legends={legends} />
           ))}
         </Row>
       </Container>
